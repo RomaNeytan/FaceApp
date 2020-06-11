d0e0c7ef193a8eb4a323cb6731aca469accc9542
RomaNeytan <57945586+RomaNeytan@users.noreply.github.com>
2020-06-11 17:43:57 +0300

    Недоработан

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,12 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-
-Vue.use(Vuex)
-
-export default new Vuex.Store({
+import Vue from "vue";
+import Vuex from "vuex";Vue.use(Vuex);export default new Vuex.Store({
   state: {
+ images: []
   },
   mutations: {
+ setImages(state, payload) {
+ state.images = payload;
+ }
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {}
+});
